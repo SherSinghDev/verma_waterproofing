5db8e50541196b382e68e66533198b38a4de4ca7
SherSinghDev <shersingh741730@gmail.com>
2025-07-16 17:18:30 +0530

    emailjs

--- js/dz.ajax.js
+++ js/dz.ajax.js
@@ -13,88 +13,81 @@
 
 **/
 
-function contactForm()
-{
-	window.verifyRecaptchaCallback = function (response) {
-        $('input[data-recaptcha]').val(response).trigger('change');
-    }
 
-    window.expiredRecaptchaCallback = function () {
-        $('input[data-recaptcha]').val("").trigger('change');
-    }
+
+function contactForm() {
+	window.verifyRecaptchaCallback = function (response) {
+		$('input[data-recaptcha]').val(response).trigger('change');
+	}
+
+	window.expiredRecaptchaCallback = function () {
+		$('input[data-recaptcha]').val("").trigger('change');
+	}
 	'use strict';
 	var msgDiv;
-	
-	$(".dzForm").on('submit',function(e){
+
+	$(".dzForm").on('submit', function (e) {
 		e.preventDefault();	//STOP default action
 		$('.dzFormMsg').html('<div class="gen alert alert-success">Submitting..</div>');
-		var dzFormAction = $(this).attr('action');
-		var dzFormData = $(this).serialize();
-		
-		$.ajax({
-			method: "POST",
-			url: dzFormAction,
-			data: dzFormData,
-			dataType: 'json',
-			success: function(dzRes){
-				if(dzRes.status == 1){
-					msgDiv = '<div class="gen alert alert-success">'+dzRes.msg+'</div>';
-				}
-				
-				if(dzRes.status == 0){
-					msgDiv = '<div class="err alert alert-danger">'+dzRes.msg+'</div>';
-				}
-				$('.dzFormMsg').html(msgDiv);
-				
-				
-				setTimeout(function(){
+		let form = e.target
+		let data = {
+			name: form.name.value,
+			email: form.email.value,
+			phone: form.phone.value,
+			subject: form.subject.value,
+			message: form.message.value,
+		}
+		emailjs.send('service_oxpshjl', 'template_xm45sov', data).then(
+			(response) => {
+				setTimeout(() => {
 					$('.dzFormMsg .alert').hide(1000);
-				}, 10000);
-				
-				$('.dzForm')[0].reset();
-                grecaptcha.reset();
-			}
-		})
+					e.target.reset()
+				}, 1000);
+			},
+			(error) => {
+				console.log('FAILED...', error);
+			},
+		);
+
 	});
-	
-	
+
+
 	/* This function is for mail champ subscription START*/
-	$(".dzSubscribe").on('submit',function(e)
-	{
+	$(".dzSubscribe").on('submit', function (e) {
 		e.preventDefault();	//STOP default action
 		var thisForm = $(this);
 		var dzFormAction = thisForm.attr('action');
 		var dzFormData = thisForm.serialize();
 		thisForm.addClass('dz-ajax-overlay');
-		
+
 		$.ajax({
 			method: "POST",
 			url: dzFormAction,
 			data: dzFormData,
 			dataType: 'json',
-		  success: function(dzRes) {
-			thisForm.removeClass('dz-ajax-overlay');  
-			if(dzRes.status == 1){
-				msgDiv = '<div class="gen alert alert-success">'+dzRes.msg+'</div>';
+			success: function (dzRes) {
+				thisForm.removeClass('dz-ajax-overlay');
+				if (dzRes.status == 1) {
+					msgDiv = '<div class="gen alert alert-success">' + dzRes.msg + '</div>';
+				}
+				if (dzRes.status == 0) {
+					msgDiv = '<div class="err alert alert-danger">' + dzRes.msg + '</div>';
+				}
+				$('.dzSubscribeMsg').html(msgDiv);
+
+				setTimeout(function () {
+					$('.dzSubscribeMsg .alert').hide(1000);
+				}, 10000);
+
+				$('.dzSubscribe')[0].reset();
 			}
-			if(dzRes.status == 0){
-				msgDiv = '<div class="err alert alert-danger">'+dzRes.msg+'</div>';
-			}
-			$('.dzSubscribeMsg').html(msgDiv);
-			
-			setTimeout(function(){
-				$('.dzSubscribeMsg .alert').hide(1000);
-			}, 10000);
-			
-			$('.dzSubscribe')[0].reset();
-		  }
-		}) 
+		})
 	});
-	
+
 	/* This function is for mail champ subscription END*/
 }
 
-jQuery(document).ready(function() {
-    'use strict';
+jQuery(document).ready(function () {
+	'use strict';
 	contactForm();
 })
